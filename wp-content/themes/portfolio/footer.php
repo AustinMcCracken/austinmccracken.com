3a8d5058259d79d5b7303cb74f35c1703b722807
Austin McCracken <austin@maxxpotential.com>
2016-09-21 15:23:56 -0400

    images and homepage update

--- wp-content/themes/portfolio/footer.php
+++ wp-content/themes/portfolio/footer.php
@@ -14,6 +14,46 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+
+		<?php
+		/**
+		 * Created by PhpStorm.
+		 * User: brian
+		 * Date: 8/26/16
+		 * Time: 12:54 PM
+		 */
+		?>
+
+		<div id="footer">
+			<?php wp_footer() ?>
+
+			<div class="row">
+				<div class="col-xs-4">
+					<a href="https://www.linkedin.com/in/brian-simoni-4b7b8810a"><img src="/wp-content/themes/simoni/images/linkedin-icon.png"></a>
+				</div>
+				<div class="col-xs-4">
+					<a href="https://github.com/briansimoni"><img src="/wp-content/themes/simoni/images/github.png"></a>
+				</div>
+				<div class="col-xs-4">
+					<a href="https://www.facebook.com/brian.simoni.5"><img src="/wp-content/themes/simoni/images/fb.png"></a>
+				</div>
+			</div>
+
+			<div class="row legal">
+				<div class="col-md-12">
+					<!-- hidden link -->
+					<p><a href="/resume">2016 Austin McCracken Theme</a></p>
+				</div>
+			</div>
+		</div><!-- #footer -->
+
+		</div><!-- #wrapper -->
+
+		<script>hljs.initHighlightingOnLoad();</script>
+		</body>
+
+		</html>
+
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'portfolio' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'portfolio' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
--- wp-content/themes/portfolio/index.php
+++ wp-content/themes/portfolio/index.php
@@ -18,36 +18,53 @@ get_header();
 	<div id="content" class="container homepage-content">
 
 		<div class="container bio">
-			<div class="col-md-12">
-				<div>
-					<a href="bio">
-						<img class="img-circle" id="bio" src="/wp-content/themes/portfolio/Images/austin.jpg">
-					</a>
+
+			<div class="col-md-6">
+				<div class="left-text">
+					<h1 class="left"><a href="bio">About Me</a></h1>
+					<p class="info-text-left">Welcome! My name is Austin McCracken, I am a 23 year old software
+						developer apprentice from Richmond, Virginia who is currently studying Computer Science
+						 at Virginia Commonwealth University! I am extremely passionate about the
+						computer science field and my work as a software developer.  I would love to show you a
+						glimpse into my work over the past few years!</p>
 				</div>
-				<p>Austin McCracken Bio</p>
+			</div>
+
+			<div class="col-md-6">
+				<a href="bio">
+					<img class="img-circle" id="bio" src="/wp-content/themes/portfolio/Images/austin.jpg">
+				</a>
+
 			</div>
 		</div>
-
 
 		<div class="container maxx">
-			<div class="col-md-12">
-				<div>
-					<a href="software">
-						<img class="img-circle" id="maxx" src="/wp-content/themes/portfolio/Images/maxx2.jpg">
-					</a>
-				</div>
-				<p>Junior Software Developer Apprentice at Maxx Potential</p>
+
+			<div class="col-md-6">
+				<a href="maxx">
+					<img class="img-circle" id="maxx" src="/wp-content/themes/portfolio/Images/maxx3.png">
+				</a>
 			</div>
+
+			<div class="col-md-6">
+				<div class="right-text">
+					<h1 class="right"><a href="maxx">Software Developer Apprentice</a></h1>
+				</div>
+			</div>
+
 		</div>
 
-		<div class="container vcu"
-			<div class="col-md-12">
-				<div>
-					<a href="compsci">
-						<img class="img-circle" id="vcu" src="/wp-content/themes/portfolio/Images/vcubig.JPG">
-					</a>
+		<div class="container vcu">
+			<div class="col-md-6">
+				<div class="left-text">
+					<h1 class="left"><a href="vcu">Computer Science at VCU</a></h1>
 				</div>
-				<p>Computer Science at Virginia Commonwealth University</p>
+			</div>
+
+			<div class="col-md-6">
+				<a href="vcu">
+					<img class="img-circle" id="vcu" src="/wp-content/themes/portfolio/Images/vcubig.JPG">
+				</a>
 			</div>
 		</div>
 
